f83d6cc42ee701b46bb2512ba12c11383c7215ce
Jeffrey Dungen <jeff@reelyactive.com>
2015-08-01 23:24:26 -0400

    Updated to Angular UI Bootstrap

--- web/js/response.js
+++ web/js/response.js
@@ -1,6 +1,6 @@
-REFRESH_MILLISECONDS = 2000;
+DEFAULT_POLLING_MILLISECONDS = 2000;
 
-angular.module('response', [])
+angular.module('response', [ 'ui.bootstrap' ])
 
   // API controller
   .controller('ApiCtrl', function($scope, $http, $interval, $window) {
@@ -8,8 +8,7 @@ angular.module('response', [])
     $scope.meta = { message: "loading", statusCode: "..." };
     $scope.links = { self: { href: url } };
     $scope.devices = {};
-
-    updateQuery();
+    $scope.expand = true;
 
     function updateQuery() {
       $http.defaults.headers.common.Accept = 'application/json';
@@ -20,10 +19,29 @@ angular.module('response', [])
           $scope.devices = data.devices;
         })
         .error(function(data, status, headers, config) {
-          $scope.meta = data._meta;
-          $scope.links = data._links;
+          $scope.meta = { message: "error", statusCode: status };
+          $scope.links = { self: { href: url } };
           $scope.devices = {};
         });
     }
-    $interval(updateQuery, REFRESH_MILLISECONDS);
+
+    $scope.isEmpty = function () {
+      return (Object.keys($scope.devices).length === 0);
+    }
+
+    $scope.updatePeriod = function (period) {
+      if(period) {
+        $scope.pollingMessage = "Polling every " + (period / 1000) + "s";
+        $interval.cancel($scope.pollingPromise);
+        $scope.pollingPromise = $interval(updateQuery, period);
+        updateQuery();
+      }
+      else {
+        $scope.pollingMessage = "Polling disabled";
+        $interval.cancel($scope.pollingPromise);
+      }
+    };
+
+    updateQuery();
+    $scope.updatePeriod(DEFAULT_POLLING_MILLISECONDS);
   });
