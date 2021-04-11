0b71e5b4dfbaa82a8a0a494b20c1180720bc8af2
jeffyactive <jeff@reelyactive.com>
2021-04-10 22:55:47 -0400

    Added human-readable raddec visualisation

--- web/barterer/js/devices.js
+++ web/barterer/js/devices.js
@@ -10,13 +10,26 @@ const STATUS_CREATED = 201;
 const STATUS_BAD_REQUEST = 400;
 const STATUS_NOT_FOUND = 404;
 const MESSAGE_BAD_REQUEST = 'Bad Request [400].  An error likely occurred on the server.';
-const MESSAGE_NOT_FOUND = 'Association for this device Not Found [404].';
+const MESSAGE_NOT_FOUND = 'Device Not Found [404].';
 const DEVICES_ROUTE = '/devices';
+const SIGNATURE_SEPARATOR = '/';
+const IDENTIFIER_TYPES = [
+    'Unknown',
+    'EUI-64',
+    'EUI-48',
+    'RND-48'
+];
+const EVENT_ICONS = [
+    'fas fa-sign-in-alt',
+    'fas fa-route',
+    'fas fa-info',
+    'fas fa-heartbeat',
+    'fas fa-sign-out-alt'
+];
 
 
 // DOM elements
 let jsonResponse = document.querySelector('#jsonResponse');
-let identifier = document.querySelector('#identifier');
 let loading = document.querySelector('#loading');
 let error = document.querySelector('#error');
 let errorMessage = document.querySelector('#errorMessage');
@@ -31,13 +44,11 @@ let deviceIdSignature = devicesUrl.substring(devicesIndex + 9);
 
 // Initialisation: GET the associations and display in DOM
 getDevices(devicesUrl, function(status, response) {
-  identifier.textContent = deviceIdSignature;
   jsonResponse.textContent = JSON.stringify(response, null, 2);
   loading.hidden = true;
 
   if(status === STATUS_OK) {
-    //deviceIdSignature = Object.keys(response.devices)[0];
-    identifier.textContent = deviceIdSignature;
+    updateDevices(response.devices);
     devices.hidden = false;
   }
   else if(status === STATUS_BAD_REQUEST) {
@@ -64,4 +75,203 @@ function getDevices(url, callback) {
   httpRequest.open('GET', url);
   httpRequest.setRequestHeader('Accept', 'application/json');
   httpRequest.send();
+}
+
+
+
+// Update the devices in the DOM
+function updateDevices(devicesList) {
+  for(const deviceSignature in devicesList) {
+    let device = devicesList[deviceSignature];
+    let deviceCard = createDeviceCard(deviceSignature, device);
+    devices.appendChild(deviceCard);
+  }
+}
+
+
+// Create the device card visualisation
+function createDeviceCard(signature, device) {
+  let card = createElement('div', 'card');
+  let header = createElement('div', 'card-header bg-dark text-white lead');
+  let idIcon = createElement('i', 'fas fa-barcode');
+  let body = createElement('div', 'card-body');
+
+  header.appendChild(idIcon);
+  header.appendChild(createElement('span', 'font-monospace', ' ' + signature));
+
+  if(device.hasOwnProperty('raddec')) {
+    let raddecCard = createRaddecCard(device.raddec);
+    body.appendChild(raddecCard);
+  }
+
+  card.appendChild(header);
+  card.appendChild(body);
+
+  return card;
+}
+
+
+// Create the raddec card visualisation
+function createRaddecCard(raddec) {
+  let card = createElement('div', 'card');
+  let body = createElement('div', 'card-body');
+  let raddecList = createRaddecList(raddec);
+  let rssiSignatureTable = createRssiSignatureTable(raddec.rssiSignature);
+  let packetsAccordion = createPacketsAccordion(raddec.packets);
+  let footer = createElement('div', 'card-footer');
+  let footerText = createElement('small', 'text-muted');
+
+  footerText.textContent = 'raddec';
+
+  body.appendChild(raddecList);
+  body.appendChild(rssiSignatureTable);
+  body.appendChild(packetsAccordion);
+  footer.appendChild(footerText);
+  card.appendChild(body);
+  card.appendChild(footer);
+
+  return card;
+}
+
+
+// Create the identifier, timestamp and events inline list visualisation
+function createRaddecList(raddec) {
+  let ul = createElement('ul', 'list-inline text-center');
+  let transmitterId = createElement('li', 'list-inline-item mx-3');
+  let transmitterIdIcon = createElement('i', 'fas fa-wifi text-ambient');
+  let transmitterIdValue = createElement('span', 'font-monospace', ' \u00a0' +
+                                                   raddec.transmitterId);
+  let transmitterIdType = createElement('li', 'list-inline-item mx-3');
+  let transmitterIdTypeIcon = createElement('i',
+                                           'fas fa-info-circle text-ambient');
+  let transmitterIdTypeValue = document.createTextNode(' \u00a0' +
+                                    IDENTIFIER_TYPES[raddec.transmitterIdType]);
+  let timestamp = createElement('li', 'list-inline-item mx-3');
+  let timestampIcon = createElement('i', 'fas fa-clock text-muted');
+  let timestampValue = document.createTextNode(' \u00a0' + 
+                               new Date(raddec.timestamp).toLocaleTimeString());
+  let events = createElement('li', 'list-inline-item mx-3');
+  let eventsIcon = createElement('i', 'fas fa-exchange-alt text-muted');
+  let eventsValue = createEventElements(raddec.events);
+
+  transmitterId.appendChild(transmitterIdIcon);
+  transmitterId.appendChild(transmitterIdValue);
+  transmitterIdType.appendChild(transmitterIdTypeIcon);
+  transmitterIdType.appendChild(transmitterIdTypeValue);
+  timestamp.appendChild(timestampIcon);
+  timestamp.appendChild(timestampValue);
+  events.appendChild(eventsIcon);
+  events.appendChild(eventsValue);
+  ul.appendChild(transmitterId);
+  ul.appendChild(transmitterIdType);
+  ul.appendChild(timestamp);
+  ul.appendChild(events);
+
+  return ul;
+}
+
+// Create the rssiSignature table visualisation
+function createRssiSignatureTable(rssiSignature) {
+  let table = createElement('table', 'table table-hover');
+  let caption = createElement('caption', null, 'rssiSignature');
+  let thead = createElement('thead');
+  let tbody = createElement('tbody');
+  let headrow = createElement('tr');
+  let receiver = createElement('th');
+  let receiverIcon = createElement('i', 'fas fa-broadcast-tower text-success');
+  let decodings = createElement('th');
+  let decodingsIcon = createElement('i', 'fas fa-hashtag text-success');
+  let rssi = createElement('th');
+  let rssiIcon = createElement('i', 'fas fa-signal text-success');
+  
+  receiver.appendChild(receiverIcon);
+  decodings.appendChild(decodingsIcon);
+  rssi.appendChild(rssiIcon);
+  headrow.appendChild(receiver);
+  headrow.appendChild(decodings);
+  headrow.appendChild(rssi);
+  thead.appendChild(headrow);
+
+  rssiSignature.forEach(function(item, index) {
+    let signature = item.receiverId + SIGNATURE_SEPARATOR + item.receiverIdType;
+    let rowClass = (index === 0) ? 'table-success' : null;
+    let row = createElement('tr', rowClass);
+    let receiverSignature = createElement('td', 'font-monospace', signature);
+    let numberOfDecodings = createElement('td', null, item.numberOfDecodings);
+    let rssiValue = createElement('td', null, item.rssi + ' dBm');
+
+    row.appendChild(receiverSignature);
+    row.appendChild(numberOfDecodings);
+    row.appendChild(rssiValue);
+    tbody.appendChild(row);
+  });
+
+  table.appendChild(caption);
+  table.appendChild(thead);
+  table.appendChild(tbody);
+
+  return table;
+}
+
+
+// Create the packets accordion visualisation
+function createPacketsAccordion(packets) {
+  let accordion = createElement('div', 'accordion accordion-flush');
+  let accordionItem = createElement('div', 'accordion-item');
+  let accordionHeader = createElement('h5', 'accordion-header');
+  let accordionButton = createElement('button', 'accordion-button collapsed',
+                                      'Packets (' + packets.length + ')');
+  let accordionCollapse = createElement('div', 'accordion-collapse collapse');
+  let accordionBody = createElement('ul', 'accordion-body list-unstyled');
+
+  packets.forEach(function(packet) {
+    let packetListItem = createElement('li', 'font-monospace user-select-all',
+                                   packet);
+    accordionBody.appendChild(packetListItem);
+  });
+
+  accordion.setAttribute('id', 'packetsAccordion');
+  accordionButton.setAttribute('data-bs-toggle', 'collapse');
+  accordionButton.setAttribute('data-bs-target', '#packetsCollapse');
+  accordionCollapse.setAttribute('id', 'packetsCollapse');
+  accordionCollapse.setAttribute('data-bs-parent', '#packetsAccordion');
+
+  accordionHeader.appendChild(accordionButton);
+  accordionCollapse.appendChild(accordionBody);
+  accordionItem.appendChild(accordionHeader);
+  accordionItem.appendChild(accordionCollapse);
+  accordion.appendChild(accordionItem);
+
+  return accordion;
+}
+
+
+// Prepare the event icons as a DocumentFragment
+function createEventElements(events) {
+  let elements = document.createDocumentFragment();
+
+  events.forEach(function(event) {
+    let space = document.createTextNode(' \u00a0');
+    let i = createElement('i', EVENT_ICONS[event]);
+    elements.appendChild(space);
+    elements.appendChild(i);
+  });
+
+  return elements;
+}
+
+
+// Create an element as specified
+function createElement(elementName, classNames, textContent) {
+  let element = document.createElement(elementName);
+
+  if(classNames) {
+    element.setAttribute('class', classNames);
+  }
+
+  if(textContent) {
+    element.textContent = textContent;
+  }
+
+  return element;
 }
